ffff8cbc9e7eeb5a426a27be461f0b49aa7818e5
Erickowski <daniel_perez_03081995@hotmail.com>
2020-05-14 23:12:14 -0500

    features desktop

--- src/components/FeatureInfo.jsx
+++ src/components/FeatureInfo.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const FeatureInfo = props => {
-    // console.log(props);
     return (
-        <>
-            <picture className="feature__picture">
-                <div className="feature__picture--background"></div>
-                <img className="feature__picture--image" src={props.features.image} alt=""/>
-            </picture>
-            <h3 className="feature__subtitle">{props.features.title}</h3>
-            <p className="feature__subdescription">{props.features.description}</p>
-        </>
+        <div className="feature__picture">
+            <div className="feature__picture--background"></div>
+            <img className="feature__picture--image" src={props.features.image} alt=""/>
+            <div>
+                <h3 className="feature__subtitle">{props.features.title}</h3>
+                <p className="feature__subdescription">{props.features.description}</p>
+                <button className="feature__button">More info</button>
+            </div>
+        </div>
     );
 }
 
