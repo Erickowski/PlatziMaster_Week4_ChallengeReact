4afa9b84dd980647dffe43c3e9d4b71aaf2c19de
Erickowski <daniel_perez_03081995@hotmail.com>
2020-05-15 12:56:09 -0500

    contact desktop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,17 +12,18 @@ class Contact extends Component {
         };
     }
     render() {
-        // const variable = "erick@gmail.com"
-        // console.log(variable.match(this.state.regex));
         return (
             <div className="contact">
                 <h3 className="contact__subtitle">35,000 + already joined</h3>
                 <h2 className="contact__title">Stay up-to-date with what we're doing</h2>
-                <div className="contact__input">
-                    <input className={this.state.error} type="text" onChange={() => {this.setState({email: event.target.value})}} placeholder="Enter your email address"/>
-                    <p className={"contact__input--error " + this.state.error}>Whoops, make sure it's an email</p>
+                <div className="contact__email">
+                    <div className="contact__email--input">
+                        <input className={this.state.error} type="text" onChange={() => {this.setState({email: event.target.value})}} placeholder="Enter your email address"/>
+                        <img className={this.state.error} src="src\assets\static\icon-error.svg" alt=""/>
+                        <p className={"contact__email--error " + this.state.error}>Whoops, make sure it's an email</p>
+                    </div>
+                    <button onClick={() => (this.state.email.match(this.state.regex) ? this.setState({error: ''}) : this.setState({error: 'error'})) } className="contact__email--button">Contact Us</button>
                 </div>
-                <button onClick={() => (this.state.email.match(this.state.regex) ? this.setState({error: ''}) : this.setState({error: 'error'})) } className="contact__button">Contact Us</button>
             </div>
         );
     }
