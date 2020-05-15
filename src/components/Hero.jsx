7cebb707d3d49ba2081e2c4608281b4083952dd7
Erickowski <daniel_perez_03081995@hotmail.com>
2020-05-14 22:36:37 -0500

    hero config flex

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,11 +9,13 @@ const Hero = () => {
                 <div className="hero__picture--background"></div>
                 <img className="hero__picture--image" src="src/assets/static/illustration-hero.svg" alt=""/>
             </picture>
-            <h1 className="hero__title">A Simple Bookmark Manager</h1>
-            <p className="hero__description">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
-            <div className="hero__buttons">
-                <button className="hero__buttons--chrome">Get it on Chrome</button>
-                <button className="hero__buttons--firefox">Get it on Firefox</button>
+            <div className="hero__content">
+                <h1 className="hero__content--title">A Simple Bookmark Manager</h1>
+                <p className="hero__content--description">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+                <div className="hero__content--buttons">
+                    <button className="chrome">Get it on Chrome</button>
+                    <button className="firefox">Get it on Firefox</button>
+                </div>
             </div>
         </section>
     );
